5e1d5301fafc3c06f2a0d88108f4ae7269d5d4a0
a.voloshin <a.voloshin@tg.dunice.net>
2024-06-20 15:27:26 +0300

    add POST

---
+++ db/20240620062501-migration-task.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        },
+        type: Sequelize.STRING,
+      },
+      completed: {
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        defaultValue: Date.now(),
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        defaultValue: Date.now(),
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Tasks');
+  },
+};
