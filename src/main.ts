4662b40ad7fd8c6b0cd5d22202c20feadfcf9cff
a.voloshin <a.voloshin@tg.dunice.net>
2024-06-28 10:57:08 +0300

    now

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-<<<<<<< HEAD
 import { ValidationPipe } from '@nestjs/common';
 
-=======
->>>>>>> main
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
     origin: true,
     methods: 'GET,PATCH,POST,DELETE',
   });
   await app.listen(process.env.APP_PORT);
-=======
-  await app.listen(3000);
->>>>>>> main
+
 }
 bootstrap();
