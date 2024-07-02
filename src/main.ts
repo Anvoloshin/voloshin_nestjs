7aedcff339bac311352efa5edf605dedc4177c2f
a.voloshin <a.voloshin@tg.dunice.net>
2024-07-02 10:09:17 +0300

    fixed v1

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: true,
+    origin: process.env.CLI_URL,
   });
   await app.listen(process.env.APP_PORT);
 }
